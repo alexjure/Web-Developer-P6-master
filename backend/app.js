40f1cb5c12fbad30d4bc76e093939dbf2d2df92e
Alex Jure <alexjuredev@gmail.com>
2023-02-08 17:03:07 +0100

    modifications rangements

--- backend/app.js
+++ backend/app.js
@@ -3,23 +3,31 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requête reçue!');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Autorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+})
 
-app.use((req, res, next) =>{
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue chef'});
-    next();
-});
-
-app.use((req, res,) =>{
-    console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/sauces', (req, res, next) => {
+    const sauces= [
+        {
+            _id: 'hello', 
+            name: 'bonjour', // String : nom de la sauce
+            manufacturer: 'héhé', //string : fabricant de la sauce
+            description: 'toto', //string: description de la sauce
+            heat: 2 , //number: nombre entre 1 et 10 décrivant la sauce
+            likes: 100, //number: nombre d'utilisateurs qui aiment (=likent) la sauce
+            dislikes: 500, //number: nombre d'utilisateurs qui n'aiment pas (=dislikes) la sauce
+            imageUrl: 'toot', //string: l'url de l'image de la sauce téléchargée par l'utilisateur
+            mainPepper: 'toto', //string: le principal ingrédient épicé de la sauce
+            usersLiked: 3, //string []: tableau des identifiants des utilisateurs qui ont aimé (=liked) la sauce
+            userDisliked: 5,//string []: tableau des identifiants des utilisateurs  qui n'ont pas aimé (=disliked) la sauce
+            userId: 'alex' //String: identifiant MongoDB unique de l'utilisateur qui a créé la sauce 
+        },
+    ];
+    res.status(200).json(sauces);
+})
 
 module.exports = app;
 
